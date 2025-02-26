820fc9a58b72db731a58f9c4f6b1156492a40a65
Sengkhim <75316691+Sengkhim@users.noreply.github.com>
2025-02-26 13:58:26 +0700

    Add Core lib

---
+++ Extensions/CoreServiceCollections.cs
@@ -0,0 +1,13 @@
+﻿using ECPLibrary.Core.Configuration;
+using ECPLibrary.Services;
+
+namespace ECPLibrary.Extensions;
+
+public static class CoreServiceCollections
+{
+
+    public static void AddCoreEcpLibrary(this IServiceCollection services)
+    {
+        services.AddScoped<IConfigurationModeling, ConfigurationModeling>();
+    }
+}
